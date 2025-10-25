de8670c64fa2f041751897bbba61d0f161278a2d
xl <lassus.xavier@gmail.com>
2025-10-25 23:40:19 +0200

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
